18356194af72bae74e9eb11e3495041783f0bee7
Deepthireddy07 <135524006+Deepthireddy07@users.noreply.github.com>
2023-12-05 13:04:39 +0530

    Update config.php

--- hospital/hms/include/config.php
+++ hospital/hms/include/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','deepusql.mysql.database.azure.com');
+define('DB_USER','deepthi');
+define('DB_PASS' ,'Suchal07052003');
 define('DB_NAME', 'hms');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
